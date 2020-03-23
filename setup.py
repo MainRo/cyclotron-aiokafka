810634c8981a5c712fdf07ed1a358374ca9b72ea
Romain Picard <romain.picard@oakbits.com>
2020-03-23 22:44:55 +0100

    Bump version: 0.0.0 → 0.1.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ install_requires = [
 
 setup(
     name="cyclotron-aiokafka",
-    version='0.0.0',
+    version='0.1.0',
     url='https://github.com/MainRo/cyclotron-aiokafka.git',
     license='MIT',
     description="AioKafka driver for cyclotron",
